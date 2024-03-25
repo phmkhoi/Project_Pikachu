c54c66ae1c013e49459e2a31080e659dff83a329
e-yee <viduydmm@gmail.com>
2024-03-25 08:49:26 +0700

    sound efffects

--- Project/Sound.cpp
+++ Project/Sound.cpp
@@ -1,7 +1,18 @@
 #include "Sound.h"
 
-void movingSound() {}
-void enterSound() {}
-void wonSound() {}
-void rightPairSound() {}
-void wrongPairSound() {}
+void arrowKeySound() {}
+void enterKeySound() {
+	PlaySound(TEXT("confirm-38513.wav"), NULL, SND_FILENAME);
+}
+void winSound() {
+	PlaySound(TEXT("winsquare-6993.wav"), NULL, SND_FILENAME);
+}
+void gameOverSound() {
+	PlaySound(TEXT("game-over-38511.wav"), NULL, SND_FILENAME);
+}
+void rightPairSound() {
+	PlaySound(TEXT("sound-fx-mario-coin_D_major.wav"), NULL, SND_FILENAME);
+}
+void wrongPairSound() {
+	PlaySound(TEXT("8 bit uh oh sound.wav"), NULL, SND_FILENAME);
+}	
